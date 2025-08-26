5598035d21641d9d7066364a6596a2356c5c3b5f
RupikaNatarajan <nrubika07@gmail.com>
2025-08-26 18:32:22 +0530

    Final Commit

--- Task3/node_app/server.js
+++ Task3/node_app/server.js
@@ -3,7 +3,7 @@ const port = 12345;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello from Node.js CI/CD</h1>");
+  res.end("<h1>Hello from Node.js CI/CD Pipeline automatically Deploys a Web App on EC2</h1>");
 });
 
 server.listen(port, () => {
